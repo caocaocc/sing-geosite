440954aa685f243b1f13798249d6bfc400593e29
caocaocc <129647241+caocaocc@users.noreply.github.com>
2026-02-13 13:36:06 +0800

    Update main.go

--- main.go
+++ main.go
@@ -422,8 +422,8 @@ func release(source string, destination string, output string, cnOutput string, 
 
 func main() {
 	err := release(
-		"v2fly/domain-list-community",
-		"sagernet/sing-geosite",
+		"caocaocc/domain-list-community",
+		"caocaocc/sing-geosite",
 		"geosite.db",
 		"geosite-cn.db",
 		"rule-set",
